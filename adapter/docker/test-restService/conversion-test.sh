78434b27bcc7998d59700dbaf80ef9df430c63cf
Pauline Dimmek <pauline.dimmek@niis.org>
2025-08-15 14:44:18 +0300

    add change requests for docker image and pipeline

--- adapter/docker/test-restService/conversion-test.sh
+++ adapter/docker/test-restService/conversion-test.sh
@@ -7,7 +7,6 @@ projectName="rest-adapter-service"
 
 docker compose -f docker-compose.yaml -p "rest-adapter-service" up -d
 trap 'RV=$?; docker compose -f docker-compose.yaml -p $projectName down -v; exit $RV' EXIT
-sleep 5
 
 docker exec $projectName-hurl-1 hurl -k --test /tmp/test/hurl-verification/format1.hurl
 docker exec $projectName-hurl-1 hurl -k --test /tmp/test/hurl-verification/format2.hurl
